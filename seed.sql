78e43f25ec9e188f1255b755d0769c0db564b631
ahawkins94 <adamhawkins94@hotmail.co.uk>
2017-10-01 17:33:36 +0100

    got the videos all displaying properly

--- seed.sql
+++ seed.sql
@@ -2,8 +2,8 @@ DROP TABLE IF EXISTS video;
 
 CREATE TABLE video (id SERIAL PRIMARY KEY, title VARCHAR, description TEXT, url TEXT, genre VARCHAR);
 
-INSERT INTO video (title, description, url, genre) VALUES ('Video 1', 'Sample Description 1', 'src="https://www.youtube.com/embed/QPCDoANDkl4"', 'Comedy');
-INSERT INTO video (title, description, url, genre) VALUES ('Video 2', 'Sample Description 2', 'src="https://www.youtube.com/embed/X16yJ6GSch0"', 'Gaming');
-INSERT INTO video (title, description, url, genre) VALUES ('Video 3', 'Sample Description 3', 'src="https://www.youtube.com/embed/98ImmFfMCFY"', 'Music');
-INSERT INTO video (title, description, url, genre) VALUES ('Video 4', 'Sample Description 4', 'src="https://www.youtube.com/embed/cdg193GvnBA"', 'Trailer');
-INSERT INTO video (title, description, url, genre) VALUES ('Video 5', 'Sample Description 5', 'src="https://www.youtube.com/embed/7dMtlvX1gWE"', 'Sports');
+INSERT INTO video (title, description, url, genre) VALUES ('Video 1', 'Sample Description 1', 'https://www.youtube.com/embed/QPCDoANDkl4', 'Comedy');
+INSERT INTO video (title, description, url, genre) VALUES ('Video 2', 'Sample Description 2', 'https://www.youtube.com/embed/X16yJ6GSch0', 'Gaming');
+INSERT INTO video (title, description, url, genre) VALUES ('Video 3', 'Sample Description 3', 'https://www.youtube.com/embed/98ImmFfMCFY', 'Music');
+INSERT INTO video (title, description, url, genre) VALUES ('Video 4', 'Sample Description 4', 'https://www.youtube.com/embed/cdg193GvnBA', 'Trailer');
+INSERT INTO video (title, description, url, genre) VALUES ('Video 5', 'Sample Description 5', 'https://www.youtube.com/embed/7dMtlvX1gWE', 'Sports');
